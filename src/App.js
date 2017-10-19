29e33a1cbe7ed9bc80fba08dd03aaf02f2cb4e67
alexandre <kalambik@gmail.com>
2017-10-19 09:56:08 +0200

    componentsMount

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
 import React, { Component } from 'react';
 import './App.css';
 import './fonts/font-awesome-4.7.0/css/font-awesome.min.css';
-/*import City, { Cities } from 'components/cities';*/
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state={}
-     //let data={}
-  };
-  
-  componentDidMount() {
+    this.state={
+      town: 'paris'
 
-    let Url= 'https://api.openweathermap.org/data/2.5/weather?q=Rennes&lang=fr&units=metric&appid=ed213ad38f67ad4fab31113e8018ab70'
-   fetch(Url,{
+    }
+  };
+
+  handleChange = function (e) {
+    e.preventDefault();
+    this.setState={
+      town: this.refs.city.ville.value
+    }
+  }
+
+  componentDidMount() {
+    
+    let Url= 'https://api.openweathermap.org/data/2.5/weather?&lang=fr&units=metric&appid=ed213ad38f67ad4fab31113e8018ab70&q=';
+   fetch(Url + this.state.town,{
       mode:'cors',
       method:'GET'
    }).then( res => res.json())
-     .then( data => {
-      console.log(data); 
+     .then( data => { 
       this.setState({
              data : data,
              iconUrl : "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
             });
+            
       });
-  }
+   }
   
+      // shouldComponentUpdate(nextState) {
+      //  const differentName = this.state.name !== nextState.name;
+      //   return differentName;
+      // }
   render() {
     
-    if (!this.state.data) {
-      return <div>Loading</div>;
-    }
+      if (!this.state.data) {
+        return <div>Loading</div>;
+      }
 
     return (
       <div className="App">
@@ -40,13 +52,13 @@ class App extends Component {
         </div>
         <i className="fa fa-sun-o" aria-hidden="true"></i>
        <section className="section">
-       {/*<form >
-          <input ref="location" placeholder='  Ville'></input>
+       <form ref='city' onSubmit={this.handleChange.bind(this)}>
+          <input name='ville' value={this.state.inputValue} placeholder='  Ville'></input>
           <input id="submitBtn" type="submit" />
-       </form>*/}
+       </form>
              <p>
                 <img src={this.state.iconUrl}></img>
-                {this.state.data.name }
+                {this.state.data.name||this.state.name }
             </p>
             <p>{this.state.data.weather[0].description }</p>
             <p>humidité: {this.state.data.main.humidity }%</p>
